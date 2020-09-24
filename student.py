43b16411f04b721f810b322ef2d270d6b9a7e552
Quinn Hartmetz <hartmetzq@gilmour.org>
2020-09-24 10:36:38 -0400

    Test Motion

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
         
-        self.right(primary=50, counter=50)
+        self.right(primary=50, counter=-50)
         time.sleep(2)
         self.stop()
 
