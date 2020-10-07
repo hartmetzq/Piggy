18f0ff1b48674887f85925d85f5566e57768c7fe
Quinn Hartmetz <hartmetzq@gilmour.org>
2020-10-07 11:19:32 -0400

    Fixing spelling error

--- student.py
+++ student.py
@@ -286,7 +286,7 @@ class Piggy(PiggyParent):
         self.servo(self.MIDPOINT)
         # so long as we see something close keep turning left 
         while self.read_distance() < self.SAFE_DISTANCE:
-            self.left(promary=40, counter=-40)
+            self.left(primary=40, counter=-40)
             time.sleep(.05)
         # stop motion before we end the method 
         self.stop()
